47c7f4be1dc0df32928da8775df81f5ab80eaab6
ZXEQzzg <112997494+ZXEQzzg@users.noreply.github.com>
2026-07-29 08:09:43 +0800

    修改vite配置
    适配

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   // base: '/ZXEQzzg_Csy.github.io/',
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  // base: '/ZXEQzzg_Csy.github.io/',
+export default defineConfig(() => {
+  // GitHub Actions 构建时自动设置这个变量
+  const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+  
+  return {
+    base: isGitHubPages ? '/ZXEQzzg_Csy.github.io/' : '/',
+    plugins: [react()],
+  };
 });
